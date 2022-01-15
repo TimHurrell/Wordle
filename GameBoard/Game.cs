5681c94190eb54e218e2a0bde49acd4c95f50968
Tim Hurrell <tim@hurrell.org>
2022-01-15 17:28:52 +0000

    Created Mark method

--- GameBoard/Game.cs
+++ GameBoard/Game.cs
@@ -4,7 +4,30 @@ namespace GameBoard
 {
     public class Board
     {
-        public char[] row = new char[5];
+        public bool Correct { get; set; } = true;
+        public char[] Row = new char[5];
+        public bool[] Guess = new bool[5];
+        
+        public Board (string word)
+        {
+            Row[0] = word[0];
+            Row[1] = word[1];
+            Row[2] = word[2];
+            Row[3] = word[3];
+            Row[4] = word[4];
+        }
+
+        public void Mark(string word)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Guess[i] = word[0] == Row[0] ? Guess[i] = true : false;
+                if (word[0] != Row[0])
+                { Correct = false; }
+
+            }
+
+        }
 
     }
 }
--- TestGameBoard/UnitTest1.cs
+++ TestGameBoard/UnitTest1.cs
@@ -10,14 +10,40 @@ namespace TestGameBoard
         public void TestWordingOnGameBoard()
         {
 
-            Board TestBoard = new Board();
-            TestBoard.row[0] = 'a';
-            TestBoard.row[1] = 'b';
-            TestBoard.row[2] = 'b';
-            TestBoard.row[3] = 'e';
-            TestBoard.row[4] = 'y';
-            Assert.Equal('e', TestBoard.row[3]);
+            //Board TestBoard = new Board();
+            //TestBoard.row[0] = 'a';
+            //TestBoard.row[1] = 'b';
+            //TestBoard.row[2] = 'b';
+            //TestBoard.row[3] = 'e';
+            //TestBoard.row[4] = 'y';
+            //Assert.Equal('e', TestBoard.row[3]);
 
         }
+
+        [Fact]
+        public void MarkRowAllWrong()
+        {
+            //using a constructor to create a Row object with a string of the answer we are looking for
+            //Row row = new Row("PILOT");
+            Board TestBoard = new Board("PILOT");
+
+            //Mark is the method which contains our logic with the parameter of our guess
+            TestBoard.Mark("AAAAA");
+
+            //Assert that a particular letter has been marked
+            Assert.False(TestBoard.Guess[0]);
+            Assert.False(TestBoard.Guess[1]);
+            Assert.False(TestBoard.Guess[2]);
+            Assert.False(TestBoard.Guess[3]);
+            Assert.False(TestBoard.Guess[4]);
+            //Assert.Equal(Mark.Wrong, row.MarkedGuess[1].Value);
+            //Assert.Equal(Mark.Wrong, row.MarkedGuess[2].Value);
+            //Assert.Equal(Mark.Wrong, row.MarkedGuess[3].Value);
+            //Assert.Equal(Mark.Wrong, row.MarkedGuess[4].Value);
+
+            //check if the row is correct or not
+            Assert.False(TestBoard.Correct);
+        }
+
     }
 }
