76df93ebfc1f56c2a6583710d6344c33ee59ffc0
Tim Hurrell <tim@hurrell.org>
2022-01-24 20:30:14 +0000

    Added more tests which work

--- GameBoard/Game.cs
+++ GameBoard/Game.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Linq;
+
 
 
 public enum Mark
@@ -14,34 +14,19 @@ namespace GameBoard
 {
     public class Row
     {
-        public bool Correct { get; set; } = true;
-        public string WordAnswer { get; set; }
-        //public char[] Row = new char[5];
-        public int[] Guess = new int[5];
+        public bool Correct { get; set; }
         public List<KeyValuePair<char, Mark>> MarkedGuess { get; set; } = new List<KeyValuePair<char, Mark>>();
 
-        //public Board (string word)
-        //{
-        //    Row[0] = word[0];
-        //    Row[1] = word[1];
-        //    Row[2] = word[2];
-        //    Row[3] = word[3];
-        //    Row[4] = word[4];
-        //    WordAnswer = word;
-        //}
-
-        //public Row()
-        //{
-        //}
 
         public void Mark(string word,string guess)
         {
+            Correct = true;
             for (int i = 0; i < 5; i++)
             {
-                //MarkedGuess[i].Key = , kvp.Value
 
                 if (word.Contains(guess[i]) && word[i] != guess[i] ) 
                 {
+                    Correct = false;
                     MarkedGuess.Add(new KeyValuePair<char, Mark>(guess[i], global::Mark.Partial));
                 }
 
--- TestGameBoard/UnitTest1.cs
+++ TestGameBoard/UnitTest1.cs
@@ -8,83 +8,20 @@ namespace TestGameBoard
     {
 
 
-        //[Fact]
-        //public void MarkRowAllWrong()
-        //{
-        //    //using a constructor to create a Row object with a string of the answer we are looking for
-        //    //Row row = new Row("PILOT");
-        //    Board TestBoard = new Board("PILOT");
 
-        //    //Mark is the method which contains our logic with the parameter of our guess
-        //    TestBoard.Mark("AAAAA");
+        [Fact]
+        public void MarkRowAllWrong()
+        {
+            Row _row = new Row();
+            _row.Mark("PILOT", "ZZZZZ");
 
-        //    //Assert that a particular letter has been marked
-        //    Assert.Equal(0, TestBoard.Guess[0]);
-        //    Assert.Equal(0, TestBoard.Guess[1]);
-        //    Assert.Equal(0, TestBoard.Guess[2]);
-        //    Assert.Equal(0, TestBoard.Guess[3]);
-        //    Assert.Equal(0, TestBoard.Guess[4]);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[1].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[2].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[3].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[4].Value);
-
-        //    //check if the row is correct or not
-        //    Assert.False(TestBoard.Correct);
-        //}
-
-
-        //[Fact]
-        //public void MarkRowWrongPlace()
-        //{
-        //    //using a constructor to create a Row object with a string of the answer we are looking for
-        //    //Row row = new Row("PILOT");
-        //    Board TestBoard = new Board("PILOT");
-
-        //    //Mark is the method which contains our logic with the parameter of our guess
-        //    TestBoard.Mark("TPILO");
-
-        //    //Assert that a particular letter has been marked
-        //    Assert.Equal(1, TestBoard.Guess[0]);
-        //    Assert.Equal(1, TestBoard.Guess[1]);
-        //    Assert.Equal(1, TestBoard.Guess[2]);
-        //    Assert.Equal(1, TestBoard.Guess[3]);
-        //    Assert.Equal(1, TestBoard.Guess[4]);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[1].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[2].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[3].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[4].Value);
-
-        //    //check if the row is correct or not
-        //    Assert.False(TestBoard.Correct);
-        //}
-
-
-        //[Fact]
-        //public void MarkRowAllCorrect()
-        //{
-        //    //using a constructor to create a Row object with a string of the answer we are looking for
-        //    //Row row = new Row("PILOT");
-        //    Board TestBoard = new Board("PILOT");
-
-        //    //Mark is the method which contains our logic with the parameter of our guess
-        //    TestBoard.Mark("PILOT");
-
-        //    //Assert that a particular letter has been marked
-        //    Assert.Equal(2, TestBoard.Guess[0]);
-        //    Assert.Equal(2, TestBoard.Guess[1]);
-        //    Assert.Equal(2, TestBoard.Guess[2]);
-        //    Assert.Equal(2, TestBoard.Guess[3]);
-        //    Assert.Equal(2, TestBoard.Guess[4]);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[1].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[2].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[3].Value);
-        //    //Assert.Equal(Mark.Wrong, row.MarkedGuess[4].Value);
-
-        //    //check if the row is correct or not
-        //    Assert.True(TestBoard.Correct);
-
-        //}
+            Assert.Equal(Mark.Wrong, _row.MarkedGuess[0].Value);
+            Assert.Equal(Mark.Wrong, _row.MarkedGuess[1].Value);
+            Assert.Equal(Mark.Wrong, _row.MarkedGuess[2].Value);
+            Assert.Equal(Mark.Wrong, _row.MarkedGuess[3].Value);
+            Assert.Equal(Mark.Wrong, _row.MarkedGuess[4].Value);
+            Assert.False(_row.Correct);
+        }
 
 
         [Fact]
@@ -101,7 +38,35 @@ namespace TestGameBoard
             Assert.False(_row.Correct);
         }
 
-        // MarkedGuess is a List<KeyValuePair<char, Mark>> or in plain words a list of key value pairs of char and the enum Mark.
+
+        [Fact]
+        public void MarkRowAllCorrectBarOne()
+        {
+            Row _row = new Row();
+            _row.Mark("PILOT", "PILOO");
+
+            Assert.Equal(Mark.Right, _row.MarkedGuess[0].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[1].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[2].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[3].Value);
+            Assert.Equal(Mark.Partial, _row.MarkedGuess[4].Value);
+            Assert.False(_row.Correct);
+        }
+
+
+        [Fact]
+        public void MarkRowAllCorrect()
+        {
+            Row _row = new Row();
+            _row.Mark("PILOT", "PILOT");
+
+            Assert.Equal(Mark.Right, _row.MarkedGuess[0].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[1].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[2].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[3].Value);
+            Assert.Equal(Mark.Right, _row.MarkedGuess[4].Value);
+            Assert.True(_row.Correct);
+        }
 
     }
 }
