f2b88e3adae9500cd522c32df9b10968ab1dd78d
Tim Hurrell <tim@hurrell.org>
2024-03-08 20:19:30 +0000

    Console updated.

--- ConsoleApp1/Console.cs
+++ ConsoleApp1/Console.cs
@@ -22,26 +22,38 @@ namespace ConsoleApp
                 WordListGenerator wordListForGameBoard = new WordListGenerator(wordlistinstance, 5); // Create another list with this list of words (why?)
                 wordListForGameBoard.RemoveWrongLength(); // remove any words that contain a number of letters other than the line above
                 string answer = wordListForGameBoard.SelectAnswer(); // select an answer from the remaining words in the list
-                Board newBoard = new Board(answer, 2); // create a game board which contains the answer and the number of attempts permitted
+                Board newBoard = new Board(answer, 5); // create a game board which contains the answer and the number of attempts permitted
                 while (newBoard.TargetNumberOfGoes == false && newBoard.GuessCorrect == false )
                 {
-                    System.Console.Write("Answer is :\n " + newBoard.Answer + "\n");
                     Row game = new Row();
                     System.Console.Write("Enter guess :\n \n");
                     string guess = System.Console.ReadLine();
                     game.Mark(newBoard.Answer, guess);
 
-                    System.Console.Write("\n " + game.MarkedGuess[0].Value + "-" + game.MarkedGuess[1].Value + "-" + game.MarkedGuess[2].Value + "-" + game.MarkedGuess[3].Value + "-" + game.MarkedGuess[4].Value + "\n");
+                    if (!wordListForGameBoard.GuessExistsInWordList(guess))
+                    {
+                        System.Console.Write(" not a permitted word or already used \n");
+                        System.Console.Write("YOU ARE DISQUALIFIED \n");
+                    }
+
+                    else
+                    {
+                        wordListForGameBoard.RemoveGuessFromWordList(guess);
+                    }
 
                     newBoard.TakeGuess(guess, game);
-                    
-                    System.Console.Write("\n \n \n You have " + (newBoard.NumberOfGoesAllowed - newBoard.ListOfRows.Count) + " goes left \n \n \n");
+                    for (int i = 0; i < newBoard.Guess.Count; i++)
+                    {
+                        System.Console.Write("\n " + newBoard.ListOfRows[i].MarkedGuess[0].Value + " " + newBoard.ListOfRows[i].MarkedGuess[1].Value + " " + newBoard.ListOfRows[i].MarkedGuess[2].Value + " " + newBoard.ListOfRows[i].MarkedGuess[3].Value + " " + newBoard.ListOfRows[i].MarkedGuess[4].Value + " " + newBoard.Guess[i].ToUpper());
+                    }
+
+                    System.Console.Write("\n \n \n You have " + (newBoard.NumberOfGoesAllowed - newBoard.ListOfRows.Count) + " goes left \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n \n");
 
 
                 }
 
 
-
+                System.Console.Write("The correct word was ....." + answer + "... :\n ");
                 System.Console.Write("Do you wish to continue? :\n ");
                 continueGame = System.Console.ReadLine();
             }
